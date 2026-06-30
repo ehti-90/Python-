5d70a757c5b01cfeeba3b0b68131419d43f72b8c
Ehtisham Hassan <ehtishamh99@gmail.com>
2026-06-30 17:17:17 +0500

    Improvements

--- Turtle_Module/snake game/main.py
+++ Turtle_Module/snake game/main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 from snake import Snake
 from food import Food
+from scoreboard import Score
 import random
 import time
 
@@ -10,11 +11,12 @@ screen = Screen()
 
 screen.bgcolor("black")
 screen.setup(800,800)
-screen.tracer(0)    # off the automatic screen update: if instead of 0 theer was 10 it would have been--
+screen.tracer(0)            # off the automatic screen update: if instead of 0 theer was 10 it would have been--
 screen.title("Snake Game")  # updated after 10 turtle movement
 
 snake = Snake()
 food = Food()
+score = Score()
 
 screen.listen()
 screen.onkey(key="Up", fun=snake.up)
@@ -22,14 +24,6 @@ screen.onkey(key="Down",fun=snake.down)
 screen.onkey(key="Left",fun=snake.left)
 screen.onkey(key="Right",fun=snake.right)
 
-# tim.shape("square")
-# tim.shapesize(1,1)
-    # tim.shearfactor(-0.5)     # slants the shape
-    # print(tim.shapetransform())
-    # (tim.tiltangle(45)) # as name suggest
-
-# pos = 0
-# all_turtles = []
     
 game_on = True
 
@@ -38,17 +32,11 @@ while game_on:
     screen.update() #   after the ops in loop is made update now
     time.sleep(0.1) # add o.2 sec delay after screen update
     
-    # for seg in range(len(all_turtles) - 1, 0, -1):   # start at lastend at 0 and take -1 steps at atime
-    #     new_x = all_turtles[seg - 1].xcor()
-    #     new_y = all_turtles[seg - 1].ycor()
-    #     all_turtles[seg].setposition(new_x, new_y)
-    
-    # all_turtles[0].forward(20)  # we made the logic in loop such that only doing anything--
-    # all_turtles[0].left(40)     # to the first object the restt follows the first
     snake.move()
     
     if snake.head.distance(food) < 15:  # this .distance check distance between snake ob and food object
-        food.new_location()              # when they get this close the  food must goto another rand place
+        food.new_location()             # when they get this close the  food must goto another rand place
+        score.update_score()      
 
 
 screen.mainloop()
